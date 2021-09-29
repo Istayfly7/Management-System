2843fd40665fdc02515ea0f9d1b2161c72d40317
Rico Porter <rico.porter@capgemini.com>
2021-09-28 18:52:45 -0600

    init commit with mvn project

---
+++ src/main/java/com/rico/ManagementSystemProject/ManagementSystemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rico.ManagementSystemProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementSystemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementSystemProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rico/ManagementSystemProject/ManagementSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rico.ManagementSystemProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSystemProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
