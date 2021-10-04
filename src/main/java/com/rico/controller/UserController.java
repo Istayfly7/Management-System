739effe8796ec5e88e9072c43aece28f0c20c027
Rico Porter <rico.porter@capgemini.com>
2021-10-04 15:25:50 -0600

    added getaccounts for admin and getmyaccount for user and admin

--- src/main/java/com/rico/controller/UserController.java
+++ src/main/java/com/rico/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.rico.controller;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.rico.entity.AdminUser;
 import com.rico.entity.User;
-import com.rico.entity.VisitorUser;
 import com.rico.repository.UserRepository;
 
 @RestController
@@ -22,6 +22,31 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	
+	@GetMapping("/accounts")
+	public ResponseEntity<List<User>> getAllAccounts(@RequestBody AdminUser account) {
+		try {
+			List<User> accountsList = new ArrayList<>();
+			
+			userRepository.findAll().forEach(accountsList::add);
+			
+			return new ResponseEntity<>(accountsList, HttpStatus.OK);
+		}
+		catch(Exception ex) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@GetMapping("/myaccount")
+	public ResponseEntity<User> getMyAccount(@RequestBody User account) {
+		try {
+			return new ResponseEntity<>(account, HttpStatus.OK);
+		}
+		catch(Exception ex) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 	
 	
 	@PostMapping("/save")
@@ -35,34 +60,5 @@ public class UserController {
 		}
 	}
 	
-	@PostMapping("/save-default")
-	public ResponseEntity<List<User>> createDefaultAccounts(){
-		try {
-			//Create a Admin by default
-			User acc1 = new AdminUser();
-			acc1.setUserName("flyHigher17");
-			acc1.setPassword("word");
-			
-			//Create 2 Visitors by default
-			User acc2 = new VisitorUser();
-			acc2.setUserName("superhotfire");
-			acc2.setPassword("1234");
-			
-			User acc3 = new VisitorUser();
-			acc3.setUserName("toosmooth");
-			acc3.setPassword("password1234");
-			
-			
-			List<User> usersList = Arrays.asList(acc1, acc2, acc3);
-			for(User users:usersList)
-				userRepository.save(users);
-			
-			return new ResponseEntity<>(usersList, HttpStatus.OK);
-			
-		}
-		catch(Exception ex) {
-			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
 	
 }
